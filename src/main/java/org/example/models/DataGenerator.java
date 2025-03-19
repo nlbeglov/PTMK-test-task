0dbd6ff1b81b34bc6ed0a04b306427797e70f315
Danil <danilrayanov@gmail.com>
2025-03-20 02:23:23 +0300

    Initial commit

---
+++ src/main/java/org/example/models/DataGenerator.java
@@ -0,0 +1,13 @@
+package org.example.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataGenerator {
+    private static final String insertSQL = "INSERT INTO users (fio, birthday, sex) VALUES (?,?,?)";
+    private static List<String> firstNames = new ArrayList<>();
+    private static List<String> lastNames = new ArrayList<>();
+    private static List<String> middleNames = new ArrayList<>();
+
+
+}
